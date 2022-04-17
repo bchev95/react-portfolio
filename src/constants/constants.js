058df3329828627f4c9e582a7d8ddf5808942c0a
bchev95 <brett.chevalier@gmail.com>
2022-04-17 18:43:18 -0400

    removed unused var

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -37,14 +37,6 @@ export const projects = [
   },
 ];
 
-export const TimeLineData = [
-  { year: 2017, text: 'Started my journey', },
-  { year: 2018, text: 'Worked as a freelance developer', },
-  { year: 2019, text: 'Founded JavaScript Mastery', },
-  { year: 2020, text: 'Shared my projects with the world', },
-  { year: 2021, text: 'Started my own platform', },
-];
-
 export const aboutData = [
   { number: 2020, text: 'Bachelor of Science, Computer Science', uni: 'University of Florida'},
   { number: 2021, text: 'Google Data Analytics Professional Certificate', },
