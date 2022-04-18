45ac0fb0a9b0072f03620e35f67510bd86ce6d37
bchev95 <brett.chevalier@gmail.com>
2022-04-17 20:45:40 -0400

    test

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ import {FaLaptopCode} from 'react-icons/fa';
 const Header = () =>  (
   <Container>
     <Div1>
-      <Link href ="/">
+      <Link href ="/react-portfolio">
         <a style={{display: "flex", alignItems: "center", color: 'white', marginBottom: 20}}>
           <FaLaptopCode size="3rem" style={{marginRight: 20}}/> <Span> My Portfolio</Span>
         </a>
@@ -15,7 +15,7 @@ const Header = () =>  (
     </Div1>
     <Div2>
       <li>
-        <Link href="#projects">
+        <Link href="react-portfolio/#projects">
           <NavLink>Projects</NavLink>
         </Link>
       </li>
