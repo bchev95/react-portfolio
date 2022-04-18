79c3a8c6fc1c0546f514b3eedbf7f16c23c9f7e0
bchev95 <brett.chevalier@gmail.com>
2022-04-17 21:32:43 -0400

    button test

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
+import { ExternalLinks } from '../Projects/ProjectsStyles';
 
 
 const Hero = () => (
