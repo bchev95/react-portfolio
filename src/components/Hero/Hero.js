a5c8bbea46a870f835fa06ccd1a19f5f7e05e8fd
bchev95 <brett.chevalier@gmail.com>
2022-04-17 21:22:09 -0400

    button test

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Link from 'next/link';
 import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
@@ -18,7 +18,7 @@ const Hero = () => (
       I love building applications, learning new technologies, and incorporating data wherever possible!
       </SectionText>
       <Link href = "https://drive.google.com/file/d/1hATz_cqT47HsOvSo9ZcBdCDkzjgPbFnl/view?usp=sharing">
-        <Button>Full Resume</Button>
+        <NavLink>Full Resume</NavLink>
       </Link>
       
     </LeftSection>
