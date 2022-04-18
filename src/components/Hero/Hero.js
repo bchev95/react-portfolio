59347f14b1cbd644609cd1f8ae6b4c0a24d81e4a
bchev95 <brett.chevalier@gmail.com>
2022-04-18 14:54:40 -0400

    button test

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
+import { ExternalLinks } from '../Projects/ProjectsStyles';
 import { LeftSection} from './HeroStyles';
-
 
 
 
@@ -15,11 +15,8 @@ const Hero = () => (
       </SectionTitle>
       <SectionText>
       I love building applications, learning new technologies, and incorporating data wherever possible!
-      </SectionText>
-        <Button>
-          <a href={"https://drive.google.com/file/d/1hATz_cqT47HsOvSo9ZcBdCDkzjgPbFnl/view?usp=sharing"} style={{color: 'white'}}>Full Resume</a>
-        </Button>
-      
+      </SectionText>   
+      <Button onClick={() => window.location.href = 'https://drive.google.com/file/d/1hATz_cqT47HsOvSo9ZcBdCDkzjgPbFnl/view?usp=sharing'}>Full Resume</Button>
     </LeftSection>
   </Section>
 );
